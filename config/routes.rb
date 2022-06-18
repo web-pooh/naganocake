ca9799eaafb95c4b07d3ae84af3b91c51999b35f
EC2 Default User <ec2-user@ip-172-31-35-221.ec2.internal>
2022-06-18 07:52:45 +0000

    sessionとRegistration以外のコントローラ]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,108 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+  end
+
+  namespace :admin do
+    get 'customers/index'
+  end
+
+  namespace :admin do
+    get 'customers/show'
+  end
+
+  namespace :admin do
+    get 'customers/edit'
+  end
+
+  namespace :admin do
+    get 'genres/index'
+  end
+
+  namespace :admin do
+    get 'genres/edit'
+  end
+
+  namespace :admin do
+    get 'items/index'
+  end
+
+  namespace :admin do
+    get 'items/new'
+  end
+
+  namespace :admin do
+    get 'items/show'
+  end
+
+  namespace :admin do
+    get 'items/edit'
+  end
+
+  namespace :admin do
+    get 'homes/top'
+  end
+
+  namespace :public do
+    get 'addresses/index'
+  end
+
+  namespace :public do
+    get 'addresses/edit'
+  end
+
+  namespace :public do
+    get 'orders/new'
+  end
+
+  namespace :public do
+    get 'orders/confirm'
+  end
+
+  namespace :public do
+    get 'orders/thanks'
+  end
+
+  namespace :public do
+    get 'orders/index'
+  end
+
+  namespace :public do
+    get 'orders/show'
+  end
+
+  namespace :public do
+    get 'carts/index'
+  end
+
+  namespace :public do
+    get 'customers/show'
+  end
+
+  namespace :public do
+    get 'customers/edit'
+  end
+
+  namespace :public do
+    get 'customers/bye'
+  end
+
+  namespace :public do
+    get 'items/index'
+  end
+
+  namespace :public do
+    get 'items/show'
+  end
+
+  namespace :public do
+    get 'homes/top'
+  end
+
+  namespace :public do
+    get 'homes/about'
+  end
+
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
