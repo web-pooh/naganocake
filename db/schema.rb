22fd2f20a2c32ebc2b6110c5c5747de73756e2a6
EC2 Default User <ec2-user@ip-172-31-16-36.ec2.internal>
2022-06-18 09:06:15 +0000

    Addressモデルの修正とマイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220618074852) do
+ActiveRecord::Schema.define(version: 20220618085230) do
 
   create_table "addresses", force: :cascade do |t|
-    t.integer  "user_id",                    null: false
+    t.integer  "customer_id",                null: false
     t.string   "postel_code",   default: "", null: false
     t.integer  "adress",                     null: false
     t.integer  "receiver_name",              null: false
