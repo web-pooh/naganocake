9ffa3b2f74f9f7d34399b6f79dac87252950dafb
EC2 Default User <ec2-user@ip-172-31-16-36.ec2.internal>
2022-06-24 04:02:31 +0000

    [add]tentative save

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,22 @@
 class Public::OrdersController < ApplicationController
+#管理者とログインユーザーのみ閲覧可
+	before_action :authenticate!
+#退会済みユーザーは閲覧不可
+ 	before_action :customer_is_deleted
+#param[:id]が取得できない場合、閲覧不可
+ 	before_action :params_check, only: [:index]
+  #customer購入情報の入力画面
+
   def new
+    @customer = current_user
+    if @customer.cart_items.blank?
+      flash[:warning] = "カートが空です"
+       redirect_to cart_items_path
+    else
+       @order = Order.new(customer_id: @customer.id)
+       @ads = @customer.ship_to_addresses
+       @ship_to_address = ShipToAddress.new(customer_id: @customer.id)
+    end
   end
 
   def confirm
