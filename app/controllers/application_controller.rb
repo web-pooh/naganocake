31b637624ec2b0661394629b9a63443af1310cde
EC2 Default User <ec2-user@ip-172-31-35-221.ec2.internal>
2022-06-24 04:00:20 +0000

    cartレイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_out_path_for(resource)
-    top_path
+    root_path
   end
 
 
@@ -16,6 +16,16 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone_number])
+  end
+  def current_cart
+    # セッションから取得したcart_idを元にCartテーブルからCart情報を取得
+    current_cart = Cart.find_by(id: session[:cart_id])
+    # Cart情報が存在しない場合、@current_cartを作成
+    current_cart = Cart.create unless current_cart
+    # 取得したCart情報よりIDを取得し、セッションに設定
+    session[:cart_id] = current_cart.id
+    # Cart情報を返却
+    current_cart
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_19_072955) do
+ActiveRecord::Schema.define(version: 2022_06_23_134535) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.integer "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -40,20 +68,19 @@ ActiveRecord::Schema.define(version: 2022_06_19_072955) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
-Customer  create_table "customers", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "first_name",             default: "",    null: false
-    t.string   "last_name",              default: "",    null: false
-    t.string   "first_name_kana",        default: "",    null: false
-    t.string   "last_name_kana",         default: "",    null: false
-    t.string   "postal_code",            default: "",    null: false
-    t.string   "address",                default: "",    null: false
-    t.string   "phone_number",           default: "",    null: false
-    t.boolean  "is_deleted",             default: false, null: false
-    t.string   "reset_password_token"
 
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.boolean "is_deleted", default: false, null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
@@ -101,4 +128,6 @@ Customer  create_table "customers", force: :cascade do |t|
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
