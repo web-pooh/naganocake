1a468c92ef93a8fead48b4d1ea93d70bb5e69d61
EC2 Default User <ec2-user@ip-172-31-16-36.ec2.internal>
2022-06-25 07:19:58 +0000

    [add]admins_customer#index

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,17 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    
   end
 
   def edit
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :lastname_kana, :first_kana, :email, :postal_code, :address, :phone_number, :is_deleted )
   end
 end
