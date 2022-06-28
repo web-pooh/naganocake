9d1221ab7e7f068b92bb466f53ef5a338d5f4e5a
EC2 Default User <ec2-user@ip-172-31-12-243.ec2.internal>
2022-06-28 03:56:58 +0000

    bootstrap0627

--- db/schema.rb
+++ db/schema.rb
@@ -110,7 +110,7 @@ ActiveRecord::Schema.define(version: 2022_06_27_040947) do
     t.integer "item_id", null: false
     t.integer "amount", null: false
     t.integer "price", null: false
-    t.integer "making_status", null: false
+    t.integer "making_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 2022_06_27_040947) do
     t.integer "billing_amount", null: false
     t.integer "postage", null: false
     t.integer "payment_method", null: false
-    t.integer "status"
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
